2099f14613baa12e1642144fce5270ab104b41f3
Sanjith S J <bettim@Sanjiths-MacBook-Air.local>
2025-06-14 15:35:03 +0530

    June 14

---
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=121 lang=python
+#
+# [121] Best Time to Buy and Sell Stock
+#
+
+# @lc code=start
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        buy = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+        return profit
+        
+# @lc code=end
+
