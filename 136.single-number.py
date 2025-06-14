34a71eb73958713bb41aa3d786f01c73936f915b
BETTlM <sanjith120806@gmail.com>
2025-06-14 21:51:06 +0530

    June 14

---
+++ 136.single-number.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=136 lang=python
+#
+# [136] Single Number
+#
+
+# @lc code=start
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        index = 0
+        for num in nums:
+            index ^= num
+        return index
+        
+# @lc code=end
+
