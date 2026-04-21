12f909abfe16589a2e8bcefb64b3d1e73254e71d
BETTlM <sanjith120806@gmail.com>
2026-04-21 11:25:54 +0530

    2026-04-21 11:25:47

---
+++ 3498.reverse-degree-of-a-string.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=3498 lang=java
+ *
+ * [3498] Reverse Degree of a String
+ */
+
+// @lc code=start
+class Solution {
+    public int reverseDegree(String s) {
+        
+    }
+}
+// @lc code=end
+
