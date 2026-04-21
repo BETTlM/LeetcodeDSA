21df77851a1163140e8992f6bb62d7c60bdb2b2e
BETTlM <sanjith120806@gmail.com>
2026-04-21 11:27:43 +0530

    2026-04-21 11:27:36

---
+++ 3498.reverse-degree-of-a-string.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=3498 lang=python
+#
+# [3498] Reverse Degree of a String
+#
+
+# @lc code=start
+class Solution(object):
+    def reverseDegree(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+# @lc code=end
+
