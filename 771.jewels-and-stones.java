176cf1b25889ac1b98ffa54f63add8476a68f84c
BETTlM <sanjith120806@gmail.com>
2026-04-21 10:40:19 +0530

    2026-04-21 10:40:12

--- 771.jewels-and-stones.java
+++ 771.jewels-and-stones.java
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        
+            HashSet<Character> jewel = new HashSet<>();
+    for (char c : jewels.toCharArray()) {
+      jewel.add(c);
+    }
+    int count = 0;
+    for (char s : stones.toCharArray()) {
+      if (jewel.contains(s)) {
+        count++;
+      }
+    }
+    return count;
     }
 }
 // @lc code=end
