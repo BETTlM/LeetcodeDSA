631f3bdf9902ad1fd8e7438700175f55633a8adf
BETTlM <sanjith120806@gmail.com>
2026-04-21 11:26:14 +0530

    2026-04-21 11:26:07

--- 3498.reverse-degree-of-a-string.java
+++ 3498.reverse-degree-of-a-string.java
@@ -7,8 +7,15 @@
 // @lc code=start
 class Solution {
     public int reverseDegree(String s) {
+    int res = 0;
+    char[] ch = s.toCharArray();
+    for (int i = 0; i < ch.length; i++) {
+        res += (i+1) * (26 - (ch[i] - 'a'));
+    }
+    return res;
         
     }
 }
+
 // @lc code=end
 
