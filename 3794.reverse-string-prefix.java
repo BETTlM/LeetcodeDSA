977dc23165da7496f76408a1d1be8fad2181a5ac
BETTlM <sanjith120806@gmail.com>
2026-04-21 10:52:39 +0530

    2026-04-21 10:52:32

---
+++ 3794.reverse-string-prefix.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=3794 lang=java
+ *
+ * [3794] Reverse String Prefix
+ */
+
+// @lc code=start
+class Solution {
+    public String reversePrefix(String s, int k) {
+        
+    }
+}
+// @lc code=end
+
