3f7c152c5724090a99f54be416002ea4263966cb
BETTlM <sanjith120806@gmail.com>
2026-04-21 10:53:17 +0530

    2026-04-21 10:53:09

--- 3794.reverse-string-prefix.java
+++ 3794.reverse-string-prefix.java
@@ -7,7 +7,17 @@
 // @lc code=start
 class Solution {
     public String reversePrefix(String s, int k) {
-        
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = k - 1;
+        while (left <= right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return new String(arr);
     }
 }
 // @lc code=end
