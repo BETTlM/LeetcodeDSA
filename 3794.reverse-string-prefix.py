e055aeeff155bdb46364534ee534dd49a5dacd19
BETTlM <sanjith120806@gmail.com>
2026-04-21 10:49:23 +0530

    2026-04-21 10:49:17

---
+++ 3794.reverse-string-prefix.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=3794 lang=python
+#
+# [3794] Reverse String Prefix
+#
+
+# @lc code=start
+class Solution(object):
+    def reversePrefix(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        
+# @lc code=end
+
